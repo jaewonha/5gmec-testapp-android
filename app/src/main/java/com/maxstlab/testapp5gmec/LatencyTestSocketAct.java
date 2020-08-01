553eca1fbbe892c88fbcb35cee2b1683052237f8
jaewnoha <jwh@t2nters.com>
2020-08-01 15:32:21 +0900

    remove tabbed template and activity swiching code is provided

---
+++ app/src/main/java/com/maxstlab/testapp5gmec/LatencyTestSocketAct.java
@@ -0,0 +1,20 @@
+package com.maxstlab.testapp5gmec;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class LatencyTestSocketAct extends Activity {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.latencytest_socket_layout);
+
+//        findViewById(R.id.btnFileDownload).setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//
+//            }
+//        });
+    }
+}
---
+++ app/src/main/java/com/maxstlab/testapp5gmec/MainAct.java
@@ -0,0 +1,34 @@
+package com.maxstlab.testapp5gmec;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainAct extends Activity implements View.OnClickListener {
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.filedownload_layout);
+
+        findViewById(R.id.btnFileDownload) .setOnClickListener(this);
+        findViewById(R.id.btnLatencySocket).setOnClickListener(this);
+        findViewById(R.id.btnLatencyPing)  .setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()) {
+            case R.id.btnFileDownload:
+                startActivity(new Intent(getApplicationContext(), FileDownloadAct.class));
+                break;
+            case R.id.btnLatencySocket:
+                startActivity(new Intent(getApplicationContext(), LatencyTestSocketAct.class));
+                break;
+            case R.id.btnLatencyPing:
+                startActivity(new Intent(getApplicationContext(), LatencyTestPingAct.class));
+                break;
+        }
+    }
+}
